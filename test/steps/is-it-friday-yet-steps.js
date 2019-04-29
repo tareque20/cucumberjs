d298ae6fcb14e060c4872c7ed180f0a88c4fcfd9
Tasfin <tareque20@gmail.com>
2019-04-29 15:47:55 +0600

    is it friday yet example

---
+++ test/steps/is-it-friday-yet-steps.js
@@ -0,0 +1,26 @@
+const { Given, When, Then } = require('cucumber');
+const { expect } = require('chai');
+
+function isItFriday(today) {
+    if (today === "Friday") {
+        return "Yes";
+    } else {
+        return "No";
+    }
+}
+
+Given('today is Sunday', function () {
+    this.today = 'Sunday';
+});
+
+Given('today is Friday', function () {
+    this.today = 'Friday';
+});
+
+When('I ask whether it\'s Friday yet', function () {
+    this.actualAnswer = isItFriday(this.today);
+});
+
+Then('I should be told {string}', function (expectedAnswer) {
+    expect(this.actualAnswer).to.equal(expectedAnswer);
+});
